ecb002ae86e916abbcf1c3e1ab1a03444f72a9e4
unknown <eddevjo@gmail.com>
2022-11-18 21:41:37 +0100

    Fixed datePicker state DataDisplay page Front-end and states

--- src/pages/DisplayData/index.js
+++ src/pages/DisplayData/index.js
@@ -1,7 +1,33 @@
 import React from "react";
-
+import style from "./index.module.scss";
+import banner from "../../images/ze.png";
+import { useLocation } from "react-router-dom";
 const DisplayData = () => {
-  return <div>DisplayData</div>;
+  const location = useLocation();
+  const data = location.state.data;
+  console.log(data.explanation);
+
+  return (
+    <div className={style.container}>
+      <div className={style.top}>
+        <div className={style.title}>{data.title}</div>
+        <div className={style.date_switch}>DEC /12 /2012</div>
+      </div>
+      <div className={style.middle}>
+        <div className={style.text}>
+          <h3>Story:</h3>
+          <p>{data.explanation}</p>
+        </div>
+        <div className={style.image}>
+          <img src={banner} />
+        </div>
+      </div>
+      <div className={style.footer}>
+        <div className={style.copyright}>copyright: Ryan Han</div>
+        <div className={style.date}>2022/08/11</div>
+      </div>
+    </div>
+  );
 };
 
 export default DisplayData;
--- src/pages/PickDate/index.js
+++ src/pages/PickDate/index.js
@@ -1,8 +1,11 @@
-import React, { useRef, useState } from "react";
-import DatePicker from "react-datepicker";
-
+import React, { useEffect, useRef, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import travel from "../../images/travel.gif";
 import style from "./index.module.scss";
+import axios from "axios";
+
 const PickDate = () => {
+  const navigate = useNavigate();
   const current = new Date();
   const today = `${current.getFullYear()}-${
     current.getMonth() + 1
@@ -14,37 +17,58 @@ const PickDate = () => {
   const Month = datearray[1];
 
   const myRef = useRef();
+  const [data, setData] = useState();
+  const fetchData = () => {
+    return axios
+      .get(
+        `https://api.nasa.gov/planetary/apod?api_key=4ZPRVlnC0rnhHkIkNaZjAgQQg9V9j8htGwJljW7o&date=${date}`
+      )
+      .then((response) => setData(response.data));
+  };
+  useEffect(() => {
+    fetchData();
+  });
 
   return (
-    <div className={style.container}>
-      Where to ?
-      <div
-        className={style.display_date}
-        onClick={() => {
-          myRef.current.showPicker();
-        }}
-      >
-        <div className={style.data}>{year}</div>
-        <div>/</div>
-        <div className={style.data}>{day}</div>
-        <div>/</div>
-        <div className={style.data}>
-          {Month}
-          <input
-            ref={myRef}
-            type="date"
-            id="start"
-            name="trip-start"
-            min="2018-01-01"
-            max={today}
-            onChange={(e) => setDate(e.target.value)}
-          />
-        </div>{" "}
+    <>
+      <div className={style.container}>
+        Where to ?
+        <div
+          className={style.display_date}
+          onClick={() => {
+            myRef.current.showPicker();
+          }}
+        >
+          <div className={style.data}>{year}</div>
+          <div>/</div>
+          <div className={style.data}>{day}</div>
+          <div>/</div>
+          <div className={style.data}>
+            {Month}
+            <input
+              ref={myRef}
+              type="date"
+              id="start"
+              name="trip-start"
+              min="2018-01-01"
+              max={today}
+              onChange={(e) => {
+                setDate(e.target.value);
+              }}
+            />
+          </div>{" "}
+        </div>
+        <div
+          className={style.proceed}
+          onClick={() => {
+            navigate("/DisplayData", { state: { data } });
+          }}
+        >
+          Go !
+        </div>
+        <img src={travel} />
       </div>
-      <div className={style.proceed} onClick={() => console.log("proceed")}>
-        Go!
-      </div>
-    </div>
+    </>
   );
 };
 
